d39deb0750f7d8cd1dc19c9b0f27282f1b9b878d
Henry Marin <henrymarin@rocketmail.com>
2020-04-18 20:13:38 -0500

    added movies route

---
+++ server/routes/routes.js
@@ -0,0 +1,16 @@
+const express = require ('express');
+const pool = require ('../modules/pool');
+const router = express.Router();
+
+//router to get all the movies from database
+router.get('/', (req, res)=>{
+    let queryText = 'SELECT * FROM "movies";';
+    console.log('Gettin all the movies', req.res);
+    pool.query(queryText).then(result =>{
+        console.log(result.row);
+        res.send(result.row);
+    }).catch(error=>{
+        console.log('issue getting all movies', error);
+        res.sendStatus(500);
+    })
+})

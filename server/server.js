cd73bf18f650b6abeafd2a1368eb423a359a2fc5
Henry Marin <henrymarin@rocketmail.com>
2020-04-18 20:18:58 -0500

    added route for database request

--- server/server.js
+++ server/server.js
@@ -3,12 +3,14 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 
+const movieRouter = require ('./routes/routes');
+
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
-
+app.use('/movies', movieRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
